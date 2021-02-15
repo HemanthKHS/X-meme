d197275db0320bed26eb817a416c6e34a03f0609
HemanthKHS <59533188+HemanthKHS@users.noreply.github.com>
2021-02-15 20:10:57 +0530

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form"
+import '../app.css'
+
+function Navbar(){
+
+    return(
+        <div className="Navbar" >
+            <Form/>
+        </div>
+    )
+    }
+
+export default Navbar
